7c1e4c62e7276d1901ea8f68029546c1070a785c
Maros Kollar <maros@k-1.com>
2014-01-27 22:38:53 +0100

    Remove old Moose exception class

--- lib/Business/UPS/Tracking/Exception.pm
+++ lib/Business/UPS/Tracking/Exception.pm
@@ -33,33 +33,4 @@ use Exception::Class(
     },
 );
 
-sub new {
-    my ( $self, @args ) = @_;
-    
-    return $self->create_error_exception(@args)->throw;
-}
-
-sub create_error_exception {
-    my ( $self, %params ) = @_;
-    
-    my $exception = Business::UPS::Tracking::X::CLASS->new( 
-        error       => $params{message},
-        method      => $params{method},
-        depth       => $params{depth},
-        evaltext    => $params{evaltext},
-        sub_name    => $params{sub_name},
-        last_error  => $params{last_error},
-        sub         => $params{sub},
-        is_require  => $params{is_require},
-        has_args    => $params{has_args},
-    );
-    $exception->{line} = $params{line};
-    $exception->{package} = $params{pack};
-    $exception->{file} = $params{file};
-    
-    return $exception;
-}
-
-
-
 1;
